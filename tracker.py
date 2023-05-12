8602e6c70684d853d2ecd4b2c23e8282009c06ef
Parker Hewitt <parkerhewitt95@gmail.com>
2023-05-12 15:38:41 -0600

    Setup structure of app

--- tracker.py
+++ tracker.py
@@ -1,1 +1,27 @@
-print("Hello, Wogrld!")
+import dash
+from dash import dcc, html
+import plotly.graph_objects as go
+import plotly.express as px
+import pandas as pd
+
+
+# Define constants and file paths
+CSV_FILE_PATH = 'NetflixViewingHistory.csv'
+
+# Load the CSV file into a Pandas DataFrame
+df = pd.read_csv(CSV_FILE_PATH)
+
+# ------------------------ Loading Data --------------------
+
+
+# ------------------------ Analyzing Data --------------------
+
+
+# ------------------------ Building Data Visualization with Dash --------------------
+
+
+#------------------------- Create the Dash application ------------------------
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
