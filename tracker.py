52e5d3f46d8412758a1b949f0ce7cc8998525428
Parker Hewitt <parkerhewitt95@gmail.com>
2023-05-12 15:58:45 -0600

    Analyzing the data that will be used in the dashboard

--- tracker.py
+++ tracker.py
@@ -32,6 +32,50 @@ df['Duration'] = pd.to_timedelta(df['Duration'], unit='m')
 
 # ------------------------ Analyzing Data --------------------
 
+# Calculate total hours watched
+hours_watched = df['Duration'].sum().total_seconds() / 3600
+
+# Calculate the number of unique content watched
+unique_content = df['Title'].nunique()
+
+# Calculate the total number of content
+total_content = df.shape[0]
+
+# Calculate watch time per show
+watch_time_per_show = df.groupby('Title')['Duration'].sum().reset_index()
+
+# Sort the Data by watch time in descending order
+watch_time_per_show = watch_time_per_show.sort_values('Duration', ascending=False)
+
+# Select the top 10 shows 
+top_10_shows = watch_time_per_show.head(10)
+
+# top 10 hours watched
+top_10_shows['Hours Watched'] = top_10_shows['Duration'].dt.total_seconds() / 3600
+
+# Calculate the shows with the most episodes watched in a single day
+most_episodes_in_one_day = df.groupby('Title')['Day'].count().reset_index()
+most_episodes_in_one_day = most_episodes_in_one_day.rename(columns={'Day': 'Record episodes in a day'})
+
+# Calculate average hours per day spent on the show
+avg_hours_per_day = df.groupby('Title').apply(lambda x: x['Duration'].sum() / x['Day'].nunique()).reset_index()
+avg_hours_per_day = avg_hours_per_day.rename(columns={0: 'Average Hours per Day'})
+
+# Combine the top 10 fastest binge watches by : most episodes in one day, and average hours per day and sort accordingly
+top_10_fastest_binges = pd.merge(top_10_shows, most_episodes_in_one_day, on='Title')
+top_10_fastest_binges = pd.merge(top_10_fastest_binges, avg_hours_per_day, on='Title')
+top_10_fastest_binges = top_10_fastest_binges.sort_values('Record episodes in a day', ascending=False) # sort table
+
+# Calculate the number of views per device
+device_views = df['Device Type'].value_counts().head(5)
+
+# Calculate watch time by weekday
+watch_time_by_weekday = df.groupby('Weekday')['Duration'].sum().reset_index()
+
+# Calculate the percentage of watch time on weekdays
+watch_time_by_weekday['Percentage'] = (watch_time_by_weekday['Duration'].dt.total_seconds() / df['Duration'].sum().total_seconds()) * 100
+
+
 
 # ------------------------ Building Data Visualization with Dash --------------------
 
